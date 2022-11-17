2f73d5b0637c506da40949008b80da8ffd7855c5
Daniel Dirksen <dirksend@usc.edu>
2022-11-17 06:05:24 +0000

    equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,13 +1,38 @@
 #include "equal-paths.h"
+#include <iostream>
 using namespace std;
 
+#define INVALID -2
+#define OUT_OF_TREE -1
+#define LEAF 0
 
-// You may add any prototypes of helper functions here
+int lengthsIfEqual(Node* root) {
+	// End of path
+	if (root == NULL) {
+		return OUT_OF_TREE;
+	}
 
+	// Get lengths of children
+	const int leftLength = lengthsIfEqual(root->left);
+	const int rightLength = lengthsIfEqual(root->right);
 
-bool equalPaths(Node * root)
-{
-    // Add your code below
+	// If ether child node is invalid
+	if (leftLength == INVALID || rightLength == INVALID) return INVALID;
 
+	if (leftLength == OUT_OF_TREE && rightLength == OUT_OF_TREE) {
+		return 1;
+	}
+
+	if (leftLength == OUT_OF_TREE) return rightLength + 1;
+	if (rightLength == OUT_OF_TREE) return leftLength + 1;
+
+	if (leftLength == rightLength) return leftLength + 1;
+	return INVALID;
+}
+
+bool equalPaths(Node* root) {
+  // returns length if equal
+	const int result = lengthsIfEqual(root);
+	return (result != INVALID);
 }
 
